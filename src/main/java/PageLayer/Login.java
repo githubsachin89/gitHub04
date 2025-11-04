21946c7f9568ddd20d7bd8720622cf2226e4d2cf
akshayrpatole-prog <akshayrpatole@gmail.com>
2025-11-04 14:51:25 +0530

    Create By Akshay

---
+++ src/main/java/PageLayer/Login.java
@@ -0,0 +1,10 @@
+package PageLayer;
+
+public class Login {
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
--- src/main/java/baseLayer/BaseClass.java
+++ src/main/java/baseLayer/BaseClass.java
@@ -1,5 +1,14 @@
 package baseLayer;
 
 public class BaseClass {
+	
+public static void main(String[] args) {
+	System.out.println("Base Class by Akshay");
+	
+}	
+
 
 }
+	
+
+
